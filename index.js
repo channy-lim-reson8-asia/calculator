52a343e3271f361a7936b8772c7d9d06945f131a
channy-lim-reson8-asia <channy.lim@reson8.asia>
2023-07-27 10:15:35 +0700

    add error handling

--- index.js
+++ index.js
@@ -14,38 +14,69 @@ mongoose
   .connect(
     "mongodb+srv://channylim:Doraemon@calculator.dsxzntd.mongodb.net/cost-overview?retryWrites=true&w=majority"
   )
-  .then(() => console.log("Connected!"));
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 app.get("/roles", async (req, res) => {
-  const roles = await Role.find();
-  res.send(roles);
+  try {
+    const roles = await Role.find();
+    res.send(roles);
+  } catch (error) {
+    res.status(500).send("Error fetching roles from the database.");
+  }
 });
+
 app.post("/roles", async (req, res) => {
-  const newRole = new Role();
-  newRole.role_name = req.body.role_name;
-  await newRole.save();
+  try {
+    const newRole = new Role({ role_name: req.body.role_name });
+    await newRole.save();
+    res.status(201).send("Role created successfully.");
+  } catch (error) {
+    res.status(500).send("Error creating role.");
+  }
 });
 
 app.get("/experience", async (req, res) => {
-  const experiences = await Experience.find();
-  res.send(experiences);
+  try {
+    const experiences = await Experience.find();
+    res.send(experiences);
+  } catch (error) {
+    res.status(500).send("Error fetching experiences from the database.");
+  }
 });
+
 app.post("/experience", async (req, res) => {
-  const newExperience = new Experience();
-  newExperience.years_of_experience = req.body.years_of_experience;
-  await newExperience.save();
+  try {
+    const newExperience = new Experience({
+      years_of_experience: req.body.years_of_experience,
+    });
+    await newExperience.save();
+    res.status(201).send("Experience created successfully.");
+  } catch (error) {
+    res.status(500).send("Error creating experience.");
+  }
 });
 
 app.get("/adds-on", async (req, res) => {
-  const addsOn = await AddsOn.find();
-  res.send(addsOn);
+  try {
+    const addsOn = await AddsOn.find();
+    res.send(addsOn);
+  } catch (error) {
+    res.status(500).send("Error fetching adds-on from the database.");
+  }
 });
+
 app.post("/adds-on", async (req, res) => {
-  const newAddsOn = new AddsOn();
-  console.log(req);
-  newAddsOn.addson_name = req.body.addson_name;
-  newAddsOn.addson_price = req.body.addson_price;
-  await newAddsOn.save();
+  try {
+    const newAddsOn = new AddsOn({
+      addson_name: req.body.addson_name,
+      addson_price: req.body.addson_price,
+    });
+    await newAddsOn.save();
+    res.status(201).send("Adds-on created successfully.");
+  } catch (error) {
+    res.status(500).send("Error creating adds-on.");
+  }
 });
 
 const PORT = process.env.PORT || 3030;
