c4773bec5cb314659ff7fab89b8b33591c9e5814
channy-lim-reson8-asia <channy.lim@reson8.asia>
2023-07-28 16:45:31 +0700

    add: status code

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const corsMiddleware = (req, res, next) => {
-  res.append("Access-Control-Allow-Origin", "*");
-  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.append("Access-Control-Allow-Headers", "Content-Type");
+  res.set({
+    "Access-Control-Allow-Origin": ["*"],
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+    "Access-Control-Allow-Headers": "Content-Type",
+  });
   next();
 };
 app.use(corsMiddleware);
