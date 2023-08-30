3fd6b01ed7eafe5ff0cdaa8c7656f6871c693be4
channy-lim-reson8-asia <channy.lim@reson8.asia>
2023-08-30 08:22:57 +0700

    env

--- index.js
+++ index.js
@@ -30,9 +30,10 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(corsMiddleware);
 
 mongoose
-  .connect(
-    "mongodb+srv://channylim:Doraemon@calculator.dsxzntd.mongodb.net/cost-overview?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB!"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
